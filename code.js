0b42a851e6bd9719a507afbcac8d64229ea93a46
veerenub6 <veerenub6@gmail.com>
2025-04-12 20:39:05 -0700

    123r123r
    rr

--- code.js
+++ code.js
@@ -5,16 +5,28 @@ async function LookUpProduct() {
     const score = document.getElementById("score");
     const tags = document.getElementById("tags");
     const alt = document.getElementById("alternatives");
+
+
     try {
         const response = await fetch('https://world.openfoodfacts.net/api/v2/product/{encodeURIcom}'); 
         const data = await response.json();
         if (data.products && data.products.length > 0) {
             const product = data.products[0];
             name.textContent = product_name;
+            tags.textContent = product_name;
             
             const nutriScore = (product.nutriscore_grade) || "what is this product";
-            const ecoScore = (product.ecoscore_grade)
+            const ecoScore = (product.ecoscore_grade) || "what is this product";
+            const  =  (product.ecoscore_grade) || 
 
+            score.textContent = "" + (nutriScore+ecoScore*4)/2;
+            alt.innerHTML = "<li> alternative </li>"
+
+        } else {
+            name.textContent = input;
+            score.textContent = "No results...";
+            tags.textContent = 
+            alt.innerHTML = "<li> alternative </li>";
         }
     }
     catch {
